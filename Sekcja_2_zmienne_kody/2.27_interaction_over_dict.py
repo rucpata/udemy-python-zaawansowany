34230dc18a996d0dac116fe8a727e0edca53f617
rucpata <rucinska.patrycja@gmail.com>
2019-10-29 16:42:10 +0100

    100% moduł 2

---
+++ Sekcja_2_zmienne_kody/2.27_interaction_over_dict.py
@@ -0,0 +1,23 @@
+banknotes_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500]
+
+dict_denominations = {}
+
+for d in banknotes_coins:
+    dict_denominations[d] = 0
+
+dict_denominations[100] += 1
+dict_denominations[20] += 1
+dict_denominations[5] += 1
+dict_denominations[0.5] += 1
+
+dict_denominations[50] += 1
+dict_denominations[20] += 2
+dict_denominations[5] += 1
+dict_denominations[2] += 2
+
+dict_denominations[100] += 1
+dict_denominations[50] += 1
+dict_denominations[2] += 1
+
+for k in sorted(dict_denominations.keys()):
+    print('Denominate: {0:6.2f} - amount {1:5}'.format(k, dict_denominations[k]))
